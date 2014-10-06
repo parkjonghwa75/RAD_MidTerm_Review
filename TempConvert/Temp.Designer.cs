9d413c2509b9b8231e8c8999f7ed0ce1a515a49b
Jaypark <Jaypark.1120@gmail.com>
2014-10-06 15:30:45 -0400

    first initiation

---
+++ TempConvert/Temp.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace TempConvert
+{
+    partial class TemperatureConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.fahrenRadio = new System.Windows.Forms.RadioButton();
+            this.celsiusRadio = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tempInFahrenTextBox = new System.Windows.Forms.TextBox();
+            this.tempInCelsiusTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperature Converter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(252, 565);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jay Park";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.inputTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(32, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(333, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Temperature";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(222, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "degrees";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(103, 40);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "&Temperature:  ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.fahrenRadio);
+            this.groupBox2.Controls.Add(this.celsiusRadio);
+            this.groupBox2.Location = new System.Drawing.Point(32, 220);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(333, 100);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Convert To";
+            // 
+            // fahrenRadio
+            // 
+            this.fahrenRadio.AutoSize = true;
+            this.fahrenRadio.Location = new System.Drawing.Point(24, 63);
+            this.fahrenRadio.Name = "fahrenRadio";
+            this.fahrenRadio.Size = new System.Drawing.Size(75, 17);
+            this.fahrenRadio.TabIndex = 1;
+            this.fahrenRadio.TabStop = true;
+            this.fahrenRadio.Text = "&Fahrenheit";
+            this.fahrenRadio.UseVisualStyleBackColor = true;
+            // 
+            // celsiusRadio
+            // 
+            this.celsiusRadio.AutoSize = true;
+            this.celsiusRadio.Location = new System.Drawing.Point(24, 30);
+            this.celsiusRadio.Name = "celsiusRadio";
+            this.celsiusRadio.Size = new System.Drawing.Size(58, 17);
+            this.celsiusRadio.TabIndex = 0;
+            this.celsiusRadio.TabStop = true;
+            this.celsiusRadio.Text = "&Celsius";
+            this.celsiusRadio.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.tempInFahrenTextBox);
+            this.groupBox3.Controls.Add(this.tempInCelsiusTextBox);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(32, 368);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(333, 105);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Output Temperature";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(273, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "degrees";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(273, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "degrees";
+            // 
+            // tempInFahrenTextBox
+            // 
+            this.tempInFahrenTextBox.Location = new System.Drawing.Point(167, 54);
+            this.tempInFahrenTextBox.Name = "tempInFahrenTextBox";
+            this.tempInFahrenTextBox.ReadOnly = true;
+            this.tempInFahrenTextBox.Size = new System.Drawing.Size(100, 20);
+            this.tempInFahrenTextBox.TabIndex = 3;
+            // 
+            // tempInCelsiusTextBox
+            // 
+            this.tempInCelsiusTextBox.Location = new System.Drawing.Point(167, 24);
+            this.tempInCelsiusTextBox.Name = "tempInCelsiusTextBox";
+            this.tempInCelsiusTextBox.ReadOnly = true;
+            this.tempInCelsiusTextBox.Size = new System.Drawing.Size(100, 20);
+            this.tempInCelsiusTextBox.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Temperature in Fahrenheit:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Temperature in Celsius: ";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(39, 526);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 5;
+            this.convertButton.Text = "C&onvert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(162, 526);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 6;
+            this.resetButton.Text = "&Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(290, 526);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // TemperatureConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(398, 605);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "TemperatureConverter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Temperature Converter";
+            this.Load += new System.EventHandler(this.TemperatureConverter_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton fahrenRadio;
+        private System.Windows.Forms.RadioButton celsiusRadio;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tempInFahrenTextBox;
+        private System.Windows.Forms.TextBox tempInCelsiusTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
